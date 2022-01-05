5665b9277e88216862cc8cd261efd889b28d6989
Tatsuya Kitabatake <e2tod2@gmail.com>
2022-01-05 12:45:05 +0900

    delete sources

--- pages/sources.tsx
+++ pages/sources.tsx
@@ -1,7 +1,8 @@
-import {gql, useQuery} from "@apollo/client";
+import {gql, useMutation, useQuery} from "@apollo/client";
 import {ReactElement} from "react";
 import SourceCreateForm from "../components/SourceCreateForm";
-import PersonRelationCreateForm from "../components/PersonRelationCreateForm";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const sourcesQuery = gql`
 query {
@@ -11,8 +12,36 @@ query {
     }
 }`;
 
+const deleteSourceQuery = gql`
+mutation DeleteSource($id: Int!) {
+    deleteSource(id: $id) {
+        id
+    }
+} 
+`;
+
 export default function Sources(): ReactElement {
     const {loading, error, data} = useQuery(sourcesQuery);
+    const [deleteSourceMutation] = useMutation(deleteSourceQuery, {
+        refetchQueries: [sourcesQuery]
+    });
+
+    const deleteSource = (source) => {
+        confirmAlert({
+            title: 'Confirm to submit',
+            message: `出典「${source.name}」を本当に削除しますか？`,
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => deleteSourceMutation({variables: {id: source.id}})
+                },
+                {
+                    label: 'No',
+                    onClick: () => {}
+                }
+            ]
+        });
+    }
 
     return (
         <div className="flex gap-x-5 w-full">
@@ -28,6 +57,7 @@ export default function Sources(): ReactElement {
                         <tr>
                             <th className="p-2">ID</th>
                             <th>名前</th>
+                            <th></th>
                         </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-100">
@@ -35,6 +65,14 @@ export default function Sources(): ReactElement {
                             <tr key={source.id}>
                                 <td className="p-2 font-medium text-gray-800">{source.id}</td>
                                 <td className="p-2 font-medium text-gray-800">{source.name}</td>
+                                <td>
+                                    <button
+                                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 text-xs rounded"
+                                        onClick={() => deleteSource(source)}
+                                    >
+                                        削除
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                         </tbody>
