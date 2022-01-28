003f0ed27bc322db39fb0400277e0d1b1e2dd9cb
Tatsuya Kitabatake <e2tod2@gmail.com>
2022-01-28 16:58:51 +0900

    chakura UI

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,168 @@
-import SideBar from "./SideBar";
+import React from 'react';
+import NextLink from "next/link";
+import {
+    IconButton,
+    Box,
+    CloseButton,
+    Flex,
+    Icon,
+    useColorModeValue,
+    Link,
+    Drawer,
+    DrawerContent,
+    Text,
+    useDisclosure,
+    BoxProps,
+    FlexProps,
+} from '@chakra-ui/react';
+import {
+    FiHome,
+    FiTrendingUp,
+    FiActivity,
+    FiMenu,
+    FiUsers,
+    FiShuffle,
+    FiBook,
+} from 'react-icons/fi';
+import { IconType } from 'react-icons';
+import { ReactText } from 'react';
+import {useRouter} from "next/router";
+
+interface LinkItemProps {
+    name: string;
+    path: string;
+    icon: IconType;
+}
+const LinkItems: Array<LinkItemProps> = [
+    { name: 'Home', path: '/', icon: FiHome },
+    { name: '人物', path: '/persons', icon: FiUsers },
+    { name: 'アクティビティ', path: '/activities', icon: FiActivity },
+    { name: '関連', path: '/person-relations', icon: FiShuffle },
+    { name: '出典', path: '/sources', icon: FiBook },
+];
 
 export default function Layout({children}) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <>
-            <div className="min-h-screen bg-gold-100">
-                <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
-                    <div className="px-5 py-5">
-                        History DB
-                    </div>
-                </nav>
-                <div className="flex overflow-hidden w-full pt-16">
-                    <SideBar/>
-                    <div id="main-content" className="h-full w-full relative overflow-y-auto lg:ml-64">
-                        <main className="p-4">
-                            {children}
-                        </main>
-                    </div>
-                </div>
-            </div>
-        </>
+        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+            <SidebarContent
+                onClose={() => onClose}
+                display={{ base: 'none', md: 'block' }}
+            />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full">
+                <DrawerContent>
+                    <SidebarContent onClose={onClose} />
+                </DrawerContent>
+            </Drawer>
+            {/*/!* mobilenav *!/*/}
+            <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
+            <Box ml={{ base: 0, md: 60 }} p="4">
+                {children}
+            </Box>
+        </Box>
     )
 }
+
+interface SidebarProps extends BoxProps {
+    onClose: () => void;
+}
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+    return (
+        <Box
+            bg={useColorModeValue('white', 'gray.900')}
+            borderRight="1px"
+            borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+            w={{ base: 'full', md: 60 }}
+            pos="fixed"
+            h="full"
+            {...rest}>
+            <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+                <Text fontSize="xl" fontFamily="monospace" fontWeight="bold">
+                    History DB
+                </Text>
+                <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+            </Flex>
+            {LinkItems.map((link, index) => (
+                <NavItem key={link.name} path={link.path} icon={link.icon}>
+                    {link.name}
+                </NavItem>
+            ))}
+        </Box>
+    );
+};
+
+interface NavItemProps extends FlexProps {
+    path: string;
+    icon: IconType;
+    children: ReactText;
+}
+const NavItem = ({ icon, children, path, ...rest }: NavItemProps) => {
+    const router = useRouter();
+    const isActive = path == router.pathname;
+    return (
+        <NextLink href={path}>
+            <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+                <Flex
+                    align="center"
+                    p="3"
+                    mx="4"
+                    my={1}
+                    borderRadius="lg"
+                    role="group"
+                    cursor="pointer"
+                    bg={isActive ? 'yellow.200' : 'inherit'}
+                    _hover={{
+                        bg: 'yellow.200'
+                    }}
+                    {...rest}>
+                    {icon && (
+                        <Icon
+                            mr="4"
+                            fontSize="16"
+                            as={icon}
+                        />
+                    )}
+                    {children}
+                </Flex>
+            </Link>
+        </NextLink>
+    );
+};
+
+
+interface MobileProps extends FlexProps {
+    onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+    return (
+        <Flex
+            ml={{ base: 0, md: 60 }}
+            px={{ base: 4, md: 24 }}
+            height="20"
+            alignItems="center"
+            bg={useColorModeValue('white', 'gray.900')}
+            borderBottomWidth="1px"
+            borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+            justifyContent="flex-start"
+            {...rest}>
+            <IconButton
+                variant="outline"
+                onClick={onOpen}
+                aria-label="open menu"
+                icon={<FiMenu />}
+            />
+
+            <Text fontSize="xl" ml="8" fontFamily="monospace" fontWeight="bold">
+                History DB
+            </Text>
+        </Flex>
+    );
+};
